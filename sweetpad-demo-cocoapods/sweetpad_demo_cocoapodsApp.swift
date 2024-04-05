1a1f83f2701f8f30d2a57ceeb466562599a74698
Yevhenii Hyzyla <hyzyla@gmail.com>
2024-04-05 23:14:43 +0200

    Initial Commit

---
+++ sweetpad-demo-cocoapods/sweetpad_demo_cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweetpad_demo_cocoapodsApp.swift
+//  sweetpad-demo-cocoapods
+//
+//  Created by Yevhenii Hyzyla on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct sweetpad_demo_cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
